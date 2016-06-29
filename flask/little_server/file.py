14626896f3467f4deebd7c924e644262259cd1ab
Francesco Ballerin <francesco@ballerin.it>
2016-06-29 14:28:14 +0200

    Almost everything edited

--- flask/little_server/file.py
+++ flask/little_server/file.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, flash, render_template
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = ''
@@ -16,24 +16,46 @@ def allowed_file(filename):
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
+        print (request.form['text'])
         if 'file' not in request.files:
             flash('No file part')
-            return redirect(request.url)
+            return 'No file part'
         file = request.files['file']
         # if user does not select file, browser also
         # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',filename=filename))
-    return "Sent"
+        try:
+            if len(file.filename)<2:
+                try:
+                    flash('No selected file')
+                    return "No selected file"
+                except:
+                    return "Unable to flash"
+        except:
+            return "Unable to work with empty file"
+        
+        try:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return 'Everything is working'
+        except:
+            return "unable to work with full file"
+    return "Not a POST request"
+
+@app.route('/data_taken', methods=['GET', 'POST'])
+def data_taken():
+    field = request.form['field']
+    result = "NOT RIPE YET"
+    return render_template('data_taken.html', field=field, result=result)
+
+@app.route('/take_data/<int:field>', methods=['GET', 'POST'])
+def take_data(field):
+    return render_template('take_data.html', field=field)
 
 @app.route('/', methods=['GET', 'POST'])
 def show_html():
-    return "<!doctype html><title>Upload new File</title><h1>Upload new File</h1><form action=\"file\" method=post enctype=multipart/form-data><p><input type=file name=file><input type=submit value=Upload></form>"
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run()
